f4347c00f426591de8e3c5871ba51f325e33fb52
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-07-07 13:06:16 -0400

    Add operator input, routes to correct function

--- script.js
+++ script.js
@@ -1,31 +1,47 @@
-let total = 0;
-let num1 = 0;
-let num2 = 0;
-
-num1 = +prompt("Please enter a number");
-num2 = +prompt("Please enter another number");
-
-console.log(add(num1, num2));
-console.log(subtract(num1, num2));
-console.log(multiply(num1, num2));
-console.log(divide(num1, num2));
+function operate(num1, num2, operator) {
+    switch(operator) {
+        case "+": 
+            add(num1, num2);
+            break;
+        case "-": 
+            subtract(num1, num2);
+            break;
+        case "*": 
+            multiply(num1, num2);
+            break;
+        case "/": 
+            divide(num1, num2);
+            break;
+    }
+}
 
 function add(num1, num2) {
     total = num1 + num2;
-    return total;
+    console.log(total);
 }
 
 function subtract(num1, num2) {
     total = num1 - num2;
-    return total; 
+    console.log(total); 
 }
 
 function multiply(num1, num2) {
     total = num1 * num2;
-    return total;
+    console.log(total);
 }
 
 function divide(num1, num2) {
     total = num1 / num2;
-    return total;
+    console.log(total);
 }
+
+let total = 0;
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+num1 = +prompt("Please enter a number");
+operator = prompt("Please enter an operator (+ , - , * , / )");
+num2 = +prompt("Please enter another number");
+
+operate(num1, num2, operator);
