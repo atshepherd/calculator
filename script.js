46103e8d63d21222f5f558fa6cf2013751622fc0
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-07-07 12:52:37 -0400

    Output math to console

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-function add() {
+let total = 0;
+let num1 = 0;
+let num2 = 0;
 
+num1 = +prompt("Please enter a number");
+num2 = +prompt("Please enter another number");
+
+console.log(add(num1, num2));
+console.log(subtract(num1, num2));
+console.log(multiply(num1, num2));
+console.log(divide(num1, num2));
+
+function add(num1, num2) {
+    total = num1 + num2;
+    return total;
 }
 
 function subtract() {
-    
+    total = num1 - num2;
+    return total; 
 }
 
 function multiply() {
-    
+    total = num1 * num2;
+    return total;
 }
 
 function divide() {
-    
+    total = num1 / num2;
+    return total;
 }
