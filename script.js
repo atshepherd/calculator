167898b9ad6bd14a44b8e4b4d0eb1e90e09765ed
alex <23156287+atshepherd@users.noreply.github.com>
2025-07-10 11:49:11 -0400

    Complete AC and Del buttons, Display numbers

--- script.js
+++ script.js
@@ -40,8 +40,46 @@ let num1 = 0;
 let num2 = 0;
 let operator = "";
 
-num1 = +prompt("Please enter a number");
+let parent = document.querySelector(".num-buttons");
+let children = parent.children;
+
+let display = document.querySelector(".display");
+
+let clearButton= document.querySelector(".clear");
+    clearButton.addEventListener("click", () => {
+        num1 = 0;
+        display.textContent = "";
+    });
+
+let deleteButton = document.querySelector(".del");
+
+    deleteButton.addEventListener("click", () => {
+        num1 = display.textContent.slice(0, -1);
+        display.textContent = num1;
+    });
+
+for (let i = 0; i < children.length; i++) {
+    children[i].addEventListener("mouseover", () => {
+        children[i].style.backgroundColor = "black";
+    });
+    children[i].addEventListener("click", () => {
+        if (num1 == 0) {
+           num1 = children[i].textContent; 
+        }
+        else num1 += children[i].textContent;
+        display.textContent = num1;
+    });
+}
+
+for (let i = 0; i < children.length; i++) {
+    children[i].addEventListener("mouseout", () => {
+        children[i].style.backgroundColor = "gray";
+    });
+}
+
+
+/* num1 = +prompt("Please enter a number");
 operator = prompt("Please enter an operator (+ , - , * , / )");
 num2 = +prompt("Please enter another number");
 
-operate(num1, num2, operator);
+operate(num1, num2, operator); */
